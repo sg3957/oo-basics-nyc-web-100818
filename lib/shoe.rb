707a6189c5019df8eaaf9c7a4b32869b4556fe23
Sydney Grant <sg3957@nyu.edu>
2018-10-09 21:33:24 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,13 @@
 class Shoe
-  attr_accessor :brand, :page_count, :genre
-  attr_reader :title
+  #attr_accessor :brand, :page_count, :genre
+  attr_reader :brand, :color, :size, :material, :condition
  
   def initialize(brand)
     @brand = brand
   end
  
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
+  # def turn_page
+  #   puts "Flipping the page...wow, you read fast!"
+  # end
  
 end
